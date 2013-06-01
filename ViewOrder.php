c1f891d755cecb5b70beddb9111baa731996bfd4
Delconi <magnadel@hotmail.com>
2013-06-01 08:44:34 +0800

    Create ViewOrder.php

---
+++ ViewOrder.php
@@ -0,0 +1,89 @@
+<?php 
+include ("connect.php");  //Addin from connect.php
+checkLogin();	//Checking if User is logged in before continuing//
+$sessionUserName = $_SESSION['userNo'];	//Retriving userno from session
+
+//Taking Order Info only from logged in user//
+$query = "SELECT * FROM ".$orderTable." WHERE ".$oT_OrderUserName." = $sessionUserName"." ORDER BY ".$oT_OrderDate." DESC";
+$result = mysqli_query($connect,$query);
+//Taking Order Info only from logged in user//
+?>
+<html>
+<head>
+<title><?php echo $websiteName." - ";	//Displaying of Website Name
+?>View all customer orders</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<!--
+************************************************************************
+Sourcerers
+Copyright (c) 2013 Del, Jordon Koh, Low Guan Hong
+Released under the GNU General Public License version 3
+
+This file is part of Sourcerers
+
+Sourcerers is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+A copy of the GNU General Public License is enclosed.
+************************************************************************
+For any enquiries, contact Del via email at magnadel@hotmail.com, Jordon Koh at jordonkoh@hotmail.com, or Low Guan Hong at grapefood@hotmail.com
+-->
+
+<body>
+<h1 id = "title">View all customer orders</h1>
+<?php
+if (isset($_GET["added"])){
+	echo "<div id = 'successAdded'>Item successfully added.</div>";	//Display successfully if User input an order
+}else if (isset($_GET['deleted'])){
+	echo "<div id = 'successAdded'>Item successfully deleted.</div>";	//Display successfully if User delete an order
+}else if (isset($_GET['edited'])){
+	echo "<div id = 'successAdded'>Item successfully edited.</div>";	//Display successfully if User edited an order
+}	
+?>
+<table>
+	<tr><b>
+		<td>Date Of Order</td>
+        <td>Customer Name</td>
+        <td>Product Name</td>
+        <td>Price Per Piece Sold</td>
+        <td>Quantity Sold</td>
+        <td>Misc Fee</td>
+        <td>Total Cost</td>
+        <td>Remarks</td>
+        </b>
+    </tr>
+    <?php while($row = mysqli_fetch_assoc($result)){
+		?><tr><?php
+			echo "<td>".$row[$oT_OrderDate]."</td>";
+			echo "<td>".$row[$oT_OrderCostumerName]."</td>";
+			
+			//Fetching Product Name from Product Table//
+			$pId = $row[$oT_OrderProductName];	
+			$queryProductName = "SELECT * FROM ".$productTable." WHERE ".$pT_ProductNumber." = $pId";	
+			$productResult = mysqli_query($connect,$queryProductName);
+			$rowcategory = mysqli_fetch_assoc($productResult);
+			//Fetching Product Name from Product Table//
+		
+			echo "<td>".$rowcategory[$pT_ProductName]."</td>";
+			echo "<td>".$row[$oT_OrderPrice]."</td>";
+			echo "<td>".$row[$oT_OrderQuantity]."</td>";
+			echo "<td>".$row[$oT_OrderMisc]."</td>";
+			echo "<td>".$row[$oT_TotalCost]."</td>";
+			echo "<td>".$row[$oT_OrderRemarks]."</td>";
+			echo "<td><a href = 'editOrder.php?oId=".$row[$oT_OrderNumber]."'>Edit</a></td>";	//Displaying of Edit Button
+			echo "<td><a href = 'deleteOrder.php?oId=".$row[$oT_OrderNumber]."'>Delete</a></td>";	//Displaying of Delete Button
+			?>
+        </tr><?php
+	}
+	?>
+</table>
+<div id = "backBtn"><a href = "index2.php">Back</a></div>
+</body>
+</html>
