f9fd6187455c210b554f58d56ea341142a76e1e5
Delconi <magnadel@hotmail.com>
2013-06-01 08:45:43 +0800

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,31 @@
+<?php
+
+session_start();  		//Start Session
+$_SESSION[] = array();		//Empty Session
+session_destroy();			//Destory Session
+
+header("location:index.php?logout=1");
+							//Send User Back To Login
+
+?>
+
+<!--
+************************************************************************
+Sourcerers
+Copyright (c) 2013 Del, Jordon Koh, Low Guan Hong
+Released under the GNU General Public License version 3
+
+This file is part of Sourcerers
+
+Sourcerers is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+A copy of the GNU General Public License is enclosed.
+************************************************************************
+For any enquiries, contact Del via email at magnadel@hotmail.com, Jordon Koh at jordonkoh@hotmail.com, or Low Guan Hong at grapefood@hotmail.com
+-->
