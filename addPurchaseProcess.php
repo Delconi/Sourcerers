345111afc557428569d83811bebd5732c4954c33
Delconi <magnadel@hotmail.com>
2013-06-04 19:40:04 +0800

    Update addPurchaseProcess.php

--- addPurchaseProcess.php
+++ addPurchaseProcess.php
@@ -23,33 +23,30 @@ For any enquiries, contact Del via email at magnadel@hotmail.com, Jordon Koh at 
 */
 
 checkLogin();	//Checking if User is logged in before continuing//
-$reDirect = "location:addPurchase.php?eField=1&dManu=".$_POST ["pManufacturer"]."&dPri=".$_POST["pPrice"]."&dQua=".$_POST ["pQuantity"]."&dShip=".$_POST["pShipping"]."&dTax=".$_POST["pTax"]."&dRem=".$_POST["pRemarks"]."&dDay=".$_POST["pDay"]."&dMon=".$_POST["pMonth"]."&dYear=".$_POST["pYear"]."&dNo=".$_POST ["pPNo"];
+$reDirect = "location:addPurchase.php?eField=1&dManu=".$_POST ["pManufacturer"]."&dPri=".$_POST["pPrice"]."&dQua=".$_POST["pQuantity"]."&dShip=".$_POST["pShipping"]."&dRem=".$_POST["pRemarks"]."&dDay=".$_POST["pDay"]."&dMon=".$_POST["pMonth"]."&dYear=".$_POST["pYear"]."&dNo=".$_POST["pPNo"];
 
-if(empty($_POST ["pPNo"])){
+if(empty($_POST["pPNo"])){
 	header($reDirect.'&ePro=1');
-	}else if(empty($_POST["pManufacturer"])||empty($_POST["pPNo"])||empty($_POST ["pQuantity"])||empty($_POST ["pPNo"])||empty($_POST["pSellPrice"])||typeCheck($_POST['pPrice'],'numeric',255)==FALSE||typeCheck($_POST['pQuantity'],'numeric',255)==FALSE||typeCheck($_POST['pSellPrice'],'numeric',255)==FALSE){
+	}else if(empty($_POST["pManufacturer"])||empty($_POST["pPNo"])||empty($_POST["pQuantity"])||empty($_POST["pPNo"])||typeCheck($_POST['pPrice'],'numeric',255)==FALSE||typeCheck($_POST['pQuantity'],'numeric',255)==FALSE){
 		header($reDirect);
 	}else{
 		//Receiving input from form//
 		$pDateInput = $_POST["pDateOfInput"];
 		$pDatePurchase = $_POST["pYear"]."-".$_POST["pMonth"]."-".$_POST["pDay"];
 		$pManufacturer = $_POST ["pManufacturer"];
-		$pProductNo = $_POST ["pPNo"]; 
+		$pProductNo = $_POST["pPNo"]; 
 		$pPricePerPiece = $_POST["pPrice"];
 		$pQuantity = $_POST ["pQuantity"];
 		if(empty($_POST["pShipping"])||typeCheck($_POST['pShipping'],'numeric',255)==FALSE)
 		{$pShipping = 0;}else
 		{$pShipping = $_POST["pShipping"];}
-		if(empty($_POST["pTax"])||typeCheck($_POST['pTax'],'numeric',255)==FALSE)
-			{$pTax = 0;}else
-			{$pTax = $_POST["pTax"];}
 		$pTotalPrice = ($pPricePerPiece*$pQuantity+$pShipping)*(100+$pTax)/100;
 		$pRemarks = $_POST["pRemarks"];
 		//Receiving input from form//
 		$sessionUserName = $_SESSION['userNo'];	//Collecting User Number from Session
 		
 		//Inserting individual value into database//
-		$query = "INSERT INTO $purchaseTable($puT_PurchaseProductNumber, $puT_PurchaseDateInput, $puT_PurchaseDate, $puT_PurchaseQuantity, $puT_PurchasePrice, $puT_PurchaseShippingPrice, $puT_TotalPurchasePrice, $puT_PurchaseRemarks, $puT_PurchaseTax, $puT_PurchaseManufacturer, $puT_PurchaseUserName) VALUES ('$pProductNo', '$pDateInput', '$pDatePurchase', '$pQuantity', '$pPricePerPiece', '$pShipping', '$pTotalPrice', '$pRemarks', '$pTax', '$pManufacturer', '$sessionUserName')";	
+		$query = "INSERT INTO $purchaseTable($puT_PurchaseProductNumber, $puT_PurchaseDateInput, $puT_PurchaseDate, $puT_PurchaseQuantity, $puT_PurchasePrice, $puT_PurchaseShippingPrice, $puT_TotalPurchasePrice, $puT_PurchaseRemarks, $puT_PurchaseManufacturer, $puT_PurchaseUserName) VALUES ('$pProductNo', '$pDateInput', '$pDatePurchase', '$pQuantity', '$pPricePerPiece', '$pShipping', '$pTotalPrice', '$pRemarks', '$pManufacturer', '$sessionUserName')";	
 		$result = mysqli_query($connect,$query);
 		//Inserting individual value into database//
 		
